e4cfeae7afd5f3ae142e8a4ce7e319e7517d6567
estef <estefania200299@hotmail.com>
2024-03-26 16:52:08 -0600

    Scanner

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -5,16 +5,15 @@ import { Link } from "react-router-dom";
 
 function Scanner(){
     const [scanResult, setScanResult] = useState(null)
-    
+
     useEffect(() =>{
         const scanner = new Html5QrcodeScanner('reader', {
             qrbox: {
-                width: 150,
-                height: 150,
+                width: 250,
+                height: 250,
             },
-            fps: 20,
-            aspectRatio: 1.3
-            
+            fps: 5,
+
         });
         
         scanner.render(success, error);
@@ -27,45 +26,30 @@ function Scanner(){
         function error(err){
             console.warn(err);
         }
-        activateCamera();
-
-        return () => {
-            scanner.clear();
-        };
     }, []);
-
-    const activateCamera = () => {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(stream => {
-                const video = document.getElementById('reader');
-                video.srcObject = stream;
-            })
-            .catch(error => {
-                console.error('Error al acceder a la cámara:', error);
-            });
-    };
 
     return (
         <div style={{ backgroundColor: '#E3E9EE' }}>
             <header>
                 <Navbar />
             </header>
-            <main className='max-w-screen-lg w-full items-center justify-between mx-auto p-4'>
-                <div className="flex flex-col pb-4">
-                    <Link to="/" onClick={() => { setStream(null); }}>
-                        <a className="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                            <img src="/left-arrow.svg" className="h-6" alt="Logo" />
-                            Regresar
-                        </a>
-                    </Link>
-                    <strong>Escanea el código</strong>
-                </div>
-                {
-                    scanResult
-                    ? <div>Éxito: <a href={scanResult}>{scanResult}</a></div>
-                    : <video id="reader" autoPlay style={{ width: '100%', height: '500px' }}></video>
-                }
+            <main className='max-w-screen-xl w-full items-center justify-between mx-auto p-4'>
+            <div className="flex flex-col">
+            <Link to="/">
+                <a className="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                <img src="/left-arrow.svg" className="h-8" alt="Logo" />
+                Regresar
+                </a>
+            </Link>
+            <strong>Escanea el código</strong>
+            </div>
+            {
+                scanResult
+                ? <div>Success: <a href={scanResult}> {scanResult} </a> </div>
+                : <div id="reader" ></div>
+            }
             </main>
+
         </div>
     );
 }
