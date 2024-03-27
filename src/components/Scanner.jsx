03ff0f95ab006746be2624aff6ff97eff236e268
estef <estefania200299@hotmail.com>
2024-03-27 12:58:37 -0600

    CAMARA CORRECCION

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,73 +1,3 @@
-// import { Html5QrcodeScanner } from "html5-qrcode";
-// import { useEffect, useState } from "react";
-// import Navbar from "./Navbar";
-// import { Link } from "react-router-dom";
-
-// function Scanner(){
-//     const [scanResult, setScanResult] = useState(null)
-
-//     let scanner = null;
-
-//     useEffect(() =>{
-//         scanner = new Html5QrcodeScanner('reader', {
-//             fps: 10,
-//             qrbox: {
-//                 width: 250, height: 250},
-//             disableFlip: true,
-//             rememberLastUsedCamera: true,
-//         });
-        
-//         scanner.render(success, error);
-
-//         function success(result) {
-//             scanner.clear();
-//             setScanResult(result);
-//         }
-
-//         function error(err){
-//             console.warn(err);
-//         }
-
-//         return () => {
-//             scanner.clear();
-//             if (scanner._currentScanRegion && scanner._currentScanRegion.cameraScanType === Html5QrcodeScanner.SCAN_TYPE_CAMERA) {
-//                 scanner._currentScanRegion.stop();
-//             }
-//         };
-//     }, []);
-
-//     const handleGoBack = () => {
-//         scanner.clear();
-//         scanner._currentScanRegion.stop()
-//     };
-
-//     return (
-//         <div style={{ backgroundColor: '#E3E9EE' }}>
-//             <header>
-//                 <Navbar />
-//             </header>
-//             <main className='max-w-screen-md w-full items-center justify-between mx-auto p-4'>
-//             <div className="flex flex-col">
-//             <Link to="/" onClick={handleGoBack}>
-//                 <a className="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline">
-//                     <img src="/left-arrow.svg" className="h-6" alt="Logo" />
-//                     Regresar
-//                 </a>
-//             </Link>
-//             <strong>Escanea el código</strong>
-//             </div>
-//             {
-//                 scanResult
-//                 ? <div>Success: <a href={scanResult}> {scanResult} </a> </div>
-//                 : <div id="reader" ></div>
-//             }
-//             </main>
-//         </div>
-//     );
-// }
-
-// export default Scanner;
-
 import { Html5QrcodeScanner } from "html5-qrcode";
 import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
@@ -79,15 +9,15 @@ function Scanner(){
     let scanner = null;
 
     useEffect(() =>{
-        const scanner = new Html5QrcodeScanner('reader', {
+        scanner = new Html5QrcodeScanner('reader', {
+            fps: 10,
             qrbox: {
-                width: 250,
-                height: 250,
-            },
-            fps: 5,
+                width: 20, height: 200},
             disableFlip: true,
             rememberLastUsedCamera: true,
-            
+            videoConstraints: {
+                facingMode: { exact: "environment" },
+            },
         });
         
         scanner.render(success, error);
@@ -101,8 +31,6 @@ function Scanner(){
             console.warn(err);
         }
 
-        activateCamera();
-
         return () => {
             scanner.clear();
             if (scanner._currentScanRegion && scanner._currentScanRegion.cameraScanType === Html5QrcodeScanner.SCAN_TYPE_CAMERA) {
@@ -110,26 +38,6 @@ function Scanner(){
             }
         };
     }, []);
-
-    const activateCamera = () => {
-        navigator.mediaDevices.enumerateDevices()
-            .then(devices => {
-                const videoDevices = devices.filter(device => device.kind === 'videoinput');
-                const rearCamera = videoDevices.find(device => device.label.includes('back') || device.label.includes('rear'));
-                if (rearCamera) {
-                    return navigator.mediaDevices.getUserMedia({ video: { deviceId: rearCamera.deviceId } });
-                } else {
-                    throw new Error('No se encontró una cámara trasera.');
-                }
-            })
-            .then(stream => {
-                const video = document.getElementById('reader');
-                video.srcObject = stream;
-            })
-            .catch(error => {
-                console.error('Error al acceder a la cámara:', error);
-            });
-    };
 
     const handleGoBack = () => {
         scanner.clear();
@@ -141,24 +49,28 @@ function Scanner(){
             <header>
                 <Navbar />
             </header>
-            <main className='max-w-screen-lg w-full items-center justify-between mx-auto p-4'>
-                <div className="flex flex-col pb-4">
-                    <Link to="/" onClick={handleGoBack}>
-                        <a className="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                            <img src="/left-arrow.svg" className="h-8" alt="Logo" />
-                            Regresar
-                        </a>
-                    </Link>
-                    <strong>Escanea el código</strong>
-                </div>
+            <main className='max-w-screen-md w-full h-screen-sm items-center justify-between mx-auto p-4'>
+            <div className="flex flex-col">
+            <Link to="/" onClick={handleGoBack}>
+                <a className="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                    <img src="/left-arrow.svg" className="h-6" alt="Logo" />
+                    Regresar
+                </a>
+            </Link>
+            <strong>Escanea el código</strong>
+            </div>
+            <div className="pt-3">
                 {
                     scanResult
-                    ? <div>Éxito: <a href={scanResult}>{scanResult}</a></div>
-                    : <video id="reader" autoPlay style={{ width: '100%', maxWidth: '400px' }}></video> 
+                    ? <div>Success: <a href={scanResult}> {scanResult} </a> </div>
+                    : <div id="reader"></div>
                 }
+            </div>
             </main>
         </div>
     );
 }
 
 export default Scanner;
+
+
